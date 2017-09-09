748a40de90a2e75ee2ee39c71a7ee3cb8f8ce293
ttwoosta <ttwoosta@gmail.com>
2017-09-09 12:00:33 -0400

    Updated the GUI.

--- Pizza Selection/frmPizzaSelection.Designer.vb
+++ Pizza Selection/frmPizzaSelection.Designer.vb
@@ -27,6 +27,10 @@ Partial Class frmPizzaSelection
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.btnDeepDish = New System.Windows.Forms.Button()
         Me.btnThinCrush = New System.Windows.Forms.Button()
+        Me.btnSelectPizza = New System.Windows.Forms.Button()
+        Me.lblIntrutions = New System.Windows.Forms.Label()
+        Me.lblConfirmation = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -43,7 +47,7 @@ Partial Class frmPizzaSelection
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(12, 74)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 46)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(185, 150)
         Me.PictureBox1.TabIndex = 1
@@ -51,7 +55,7 @@ Partial Class frmPizzaSelection
         '
         'PictureBox2
         '
-        Me.PictureBox2.Location = New System.Drawing.Point(215, 74)
+        Me.PictureBox2.Location = New System.Drawing.Point(215, 46)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(185, 150)
         Me.PictureBox2.TabIndex = 2
@@ -59,7 +63,7 @@ Partial Class frmPizzaSelection
         '
         'btnDeepDish
         '
-        Me.btnDeepDish.Location = New System.Drawing.Point(52, 246)
+        Me.btnDeepDish.Location = New System.Drawing.Point(47, 214)
         Me.btnDeepDish.Name = "btnDeepDish"
         Me.btnDeepDish.Size = New System.Drawing.Size(104, 23)
         Me.btnDeepDish.TabIndex = 3
@@ -68,18 +72,59 @@ Partial Class frmPizzaSelection
         '
         'btnThinCrush
         '
-        Me.btnThinCrush.Location = New System.Drawing.Point(52, 295)
+        Me.btnThinCrush.Location = New System.Drawing.Point(267, 214)
         Me.btnThinCrush.Name = "btnThinCrush"
         Me.btnThinCrush.Size = New System.Drawing.Size(104, 23)
         Me.btnThinCrush.TabIndex = 4
         Me.btnThinCrush.Text = "Thin Crust"
         Me.btnThinCrush.UseVisualStyleBackColor = True
         '
+        'btnSelectPizza
+        '
+        Me.btnSelectPizza.Location = New System.Drawing.Point(157, 214)
+        Me.btnSelectPizza.Name = "btnSelectPizza"
+        Me.btnSelectPizza.Size = New System.Drawing.Size(104, 23)
+        Me.btnSelectPizza.TabIndex = 5
+        Me.btnSelectPizza.Text = "Select Pizza"
+        Me.btnSelectPizza.UseVisualStyleBackColor = True
+        '
+        'lblIntrutions
+        '
+        Me.lblIntrutions.AutoSize = True
+        Me.lblIntrutions.Location = New System.Drawing.Point(34, 252)
+        Me.lblIntrutions.Name = "lblIntrutions"
+        Me.lblIntrutions.Size = New System.Drawing.Size(345, 17)
+        Me.lblIntrutions.TabIndex = 6
+        Me.lblIntrutions.Text = "Choose a pizza and then click the Select Pizza button"
+        '
+        'lblConfirmation
+        '
+        Me.lblConfirmation.AutoSize = True
+        Me.lblConfirmation.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConfirmation.Location = New System.Drawing.Point(119, 280)
+        Me.lblConfirmation.Name = "lblConfirmation"
+        Me.lblConfirmation.Size = New System.Drawing.Size(175, 18)
+        Me.lblConfirmation.TabIndex = 7
+        Me.lblConfirmation.Text = "Enjoy your pizza selection"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(169, 313)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit Window"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
         'frmPizzaSelection
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(412, 348)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblConfirmation)
+        Me.Controls.Add(Me.lblIntrutions)
+        Me.Controls.Add(Me.btnSelectPizza)
         Me.Controls.Add(Me.btnThinCrush)
         Me.Controls.Add(Me.btnDeepDish)
         Me.Controls.Add(Me.PictureBox2)
@@ -99,4 +144,8 @@ Partial Class frmPizzaSelection
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents btnDeepDish As Button
     Friend WithEvents btnThinCrush As Button
+    Friend WithEvents btnSelectPizza As Button
+    Friend WithEvents lblIntrutions As Label
+    Friend WithEvents lblConfirmation As Label
+    Friend WithEvents btnExit As Button
 End Class
